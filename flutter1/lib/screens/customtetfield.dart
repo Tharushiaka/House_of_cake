ac4f8fdb4058b0b8c24698d26c79ac0eb6c46617
ShashiniU <uwanshika1997@gmail.com>
2020-12-29 21:06:06 +0530

    customtextfield add

---
+++ flutter1/lib/screens/customtetfield.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
