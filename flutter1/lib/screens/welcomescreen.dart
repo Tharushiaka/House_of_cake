84b64cddd36def6cd9c153aa9b82e973c7afac95
ShashiniU <uwanshika1997@gmail.com>
2020-12-30 20:51:46 +0530

    frontpage commit

--- flutter1/lib/screens/welcomescreen.dart
+++ flutter1/lib/screens/welcomescreen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -11,11 +13,125 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [Colors.pink[100], Colors.pink[50], Colors.white],
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
-        )),
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.white, Colors.white],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              Image.asset(
+                'assets/images/logoIcon.png',
+                width: 200,
+                height: 200,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'HOUSE OF CAKE',
+                style: TextStyle(
+                  color: Colors.red[900],
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Welcome to HOUSE OF CAKE',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'The Easiest Way to Send Cakes and Gifts for Your Loves Ones!',
+                maxLines: 2,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 17,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 75,
+                  right: 75,
+                ),
+                child: ButtonTheme(
+                  minWidth: MediaQuery.of(context).size.width,
+                  height: 45,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    color: Colors.pink,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 75,
+                  right: 75,
+                ),
+                child: ButtonTheme(
+                  minWidth: MediaQuery.of(context).size.width,
+                  height: 45,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    color: Colors.pink,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Text(
+                      'SignUp',
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text(
+                      'ADMIN LOGIN',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 22,
+                          decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
