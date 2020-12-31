eeab80d4b733489f99f9139b9b2cb1d22ac8c939
dpgehani20 <69641659+dpgehani20@users.noreply.github.com>
2020-12-31 12:34:58 +0530

    add img,drawer,horizo list

---
+++ flutter1/lib/homescreen/cakepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CakePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('cake page'),
+        backgroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+}
